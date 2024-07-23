69ba8e3d517e4c36fad8a8d7f22fa1daa5755a83
miriamew <147558257+miriamew@users.noreply.github.com>
2024-07-22 21:34:06 -0400

    Add files via upload

---
+++ BloomerangGrandparentExport.py
@@ -0,0 +1,99 @@
+from pip._vendor import requests
+import os
+import csv
+import time
+os.chdir(r"c:\Users\miria\Downloads")
+#Dealing with CSV file called PopuliDonations
+f = open('BloomerangExport.csv', 'r')
+reader = csv.reader(f)
+mylist = list(reader)
+f.close()
+
+alreadyHouseholds = []
+row = 0
+mylist = [["Sort Name","Recognition Name","His Mobile Phone Number","Her Mobile Phone Number","Home Phone Number","Primary Email Address", "Primary Street","Primary City","Primary State","Primary ZIP Code","Acct #"]]
+upto = 0
+while upto < 78:
+    print(upto)
+    response3 = requests.get('https://api.bloomerang.co/v2/households?skip='+ str(50*upto)+'&take=50',
+    headers={
+    'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+    },
+    json={
+    }
+    )
+    for person in (response3.json()["Results"]):
+        added = False
+        shouldadd = False
+        sortName = recName = hiscell = hercell = homephone = email = street=city=state=zip=acct=""
+        
+        householdlist = []
+        try:
+            for p in person['MemberIds']:
+                head = False
+                response1 = requests.get('https://api.bloomerang.co/v2/constituent/' + str(p),
+                headers={
+                'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+                },
+                json={
+                }
+                )
+                for thing in response1.json()['CustomValues']:
+                    if thing["FieldId"] == 18432:
+                        for t in thing['Values']:
+                            try:
+                                if ((t['Id'] == 19472) or (t['Id'] == 19471)) and (response1.json()['PrimaryAddress']["State"]) == "Maryland":
+                                    shouldadd = True
+                                    print(response1.json())
+                                    lastname = (response1.json()['LastName'])
+                                    acct = response1.json()['AccountNumber']
+                                    if (response1.json()['IsHeadOfHousehold']):
+                                        head = True
+                                    try:
+                                        email =response1.json()['PrimaryEmail']['Value']
+                                    except:
+                                        pass
+                            
+                                    street = response1.json()['PrimaryAddress']["Street"]
+                                    city = response1.json()['PrimaryAddress']["City"]
+                                    state= response1.json()['PrimaryAddress']["State"]
+                                    zip= response1.json()['PrimaryAddress']["PostalCode"]
+                                    for phone in (response1.json()["PhoneIds"]):
+                                        phoneresponse = requests.get('https://api.bloomerang.co/v2/phone/' + str(phone),
+                                        headers={
+                                        'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+                                        },
+                                        json={
+                                        }
+                                        )
+                                        if (phoneresponse.json()["Type"]) == "Home":
+                                            homephone = (phoneresponse.json()["Number"])
+                                            print(homephone)
+                                        if ((phoneresponse.json()["Type"]) == "Mobile") and (head):
+                                            print(hiscell)
+                                            hiscell = (phoneresponse.json()["Number"])
+                                        elif ((phoneresponse.json()["Type"]) == "Mobile"):
+                                            print(hercell)
+                                            hercell = (phoneresponse.json()["Number"])
+
+                                    householdlist= [person['SortName'] ,person['RecognitionName'], hiscell ,hercell , homephone , email ,street,city,state,zip,acct]
+                            except:
+                                pass
+        except:
+            pass
+        if added == False and shouldadd == True:
+            print(householdlist)
+            mylist.append(householdlist)
+            added = True
+                        
+                        
+                        
+        
+    
+    file3 = open('BloomerangExport.csv', 'w', newline = '')
+    csv_writer = csv.writer(file3)
+    csv_writer.writerows(mylist)
+    #print(mylist)
+    file3.close()
+    upto = upto+1
+
---
+++ uploadDonations2024.py
@@ -0,0 +1,102 @@
+from pip._vendor import requests
+import os
+import csv
+os.chdir(r"c:\Users\miria\Downloads")
+'''#Dealing with CSV file called PopuliDonations
+f = open('BloomerangExport.csv', 'r')
+reader = csv.reader(f)
+mylist = list(reader)
+f.close()'''
+
+alreadyHouseholds = []
+row = 0
+mylist = [["Name","Last Name","His Cell Phone","Her Cell Phone","Home Phone", "His Email","Her Email","Address"]]
+upto = 0
+while upto < 1:
+    response3 = requests.get('https://api.bloomerang.co/v2/households?skip='+ str(50*upto)+'&take=50',
+    headers={
+    'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+    },
+    json={
+        
+    }
+    )
+
+    for person in (response3.json()["Results"]):
+        lastname = hiscell = hercell = homephone = hisemail = heremail = address = ""
+        householdlist = []
+        
+        #print(person)
+        for p in person['MemberIds']:
+            head = False
+            response1 = requests.get('https://api.bloomerang.co/v2/constituent/' + str(p),
+            headers={
+            'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+            },
+            json={
+                
+            }
+            )
+            lastname = (response1.json()['LastName'])
+            print(response1.json())
+            if (response1.json()['IsHeadOfHousehold']):
+                head = True
+            try:
+                address = (response1.json()['PrimaryAddress']["Street"] + " "+response1.json()['PrimaryAddress']["City"]+" "+response1.json()['PrimaryAddress']["State"])
+                if (head):
+                    hisemail = (response1.json()['PrimaryEmail']["Value"])
+                else:
+                    heremail = (response1.json()['PrimaryEmail']["Value"])
+                #print(response1.json()['PrimaryPhone']["Number"])
+                for phone in (response1.json()["PhoneIds"]):
+                    phoneresponse = requests.get('https://api.bloomerang.co/v2/phone/' + str(phone),
+                    headers={
+                    'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+                    },
+                    json={
+                        
+                    }
+                    )
+                    if (phoneresponse.json()["Type"]) == "Home":
+                        homephone = (phoneresponse.json()["Number"])
+                    if ((phoneresponse.json()["Type"]) == "Mobile") and (head):
+                        hiscell = (phoneresponse.json()["Number"])
+                    elif ((phoneresponse.json()["Type"]) == "Mobile"):
+                        hercell = (phoneresponse.json()["Number"])
+        
+
+                '''for phone in (response1.json()["EmailIds"]):
+                    phoneresponse = requests.get('https://api.bloomerang.co/v2/email/' + str(phone),
+                    headers={
+                    'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+                    },
+                    json={
+                        
+                    }
+                    )
+                    print(phoneresponse.json())'''
+                '''for phone in (response1.json()["AddressIds"]):
+                    phoneresponse = requests.get('https://api.bloomerang.co/v2/address/' + str(phone),
+                    headers={
+                    'X-API-KEY': '6404fa5b-486a-8857-40f2-ac89b86b400c'
+                    },
+                    json={
+                        
+                    }
+                    )
+                    print(phoneresponse.json())'''
+            except:
+                pass
+        householdlist= [person['RecognitionName'], lastname, hiscell, hercell, homephone, hisemail, heremail, address]
+        print(mylist)
+        mylist.append(householdlist)
+        
+        
+    
+    '''file3 = open('BloomerangExport.csv', 'w', newline = '')
+    csv_writer = csv.writer(file3)
+    csv_writer.writerows(mylist)
+    #print(mylist)
+    file3.close()
+    upto = upto+1'''
+
